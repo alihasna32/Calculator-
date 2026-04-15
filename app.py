dae8f987910df13885890a9fc03751e20c6803c7
alihasna32 <muktermukter55@gmail.com>
2026-04-15 22:34:04 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    expression = ""
+    result = ""
+    
+    if request.method == 'POST':
+        expression = request.form.get('expression', '')
+        action = request.form.get('action', '')
+        
+        if action == 'calculate':
+            try:
+                result = str(eval(expression))
+                expression = result  # ফলাফলকে নতুন expression হিসেবে রাখে
+            except:
+                result = "Error"
+        elif action == 'clear':
+            expression = ""
+        elif action == 'delete':
+            expression = expression[:-1]
+        else:
+            # সংখ্যা বা অপারেটর যোগ করা
+            expression += action
+    
+    return render_template('index.html', expression=expression, result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
